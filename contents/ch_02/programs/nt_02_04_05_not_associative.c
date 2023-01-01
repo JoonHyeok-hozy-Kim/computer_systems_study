a694abd7293c4a2088ee5a3420ef0ed802b1a47c
HOzy-Joon <danielisdan@naver.com>
2023-01-02 00:48:59 +0900

    2.4.5 covered

---
+++ contents/ch_02/programs/nt_02_04_05_not_associative.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void not_associative_test(float a, float b, float c, float d){
+    float x, y, t;
+    x = a + b + c;
+    y = b + c + d;
+    printf("Case 1) x = %.50f, y = %.50f\n", x, y);
+    t = b + c;
+    x = a + t;
+    y = t + d;
+    printf("Case 2) x = %.50f, y = %.50f\n", x, y);
+}
+
+int main() {
+    not_associative_test(3.14, 1e10, -1e10, 2.0);
+
+    printf("%f\n", 1e20 * (1e20 * 1e-20));
+    printf("%f\n", (1e20 * 1e20) * 1e-20);
+}
