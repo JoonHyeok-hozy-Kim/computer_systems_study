7dc81eb284eb878df551bdff74e4289055fabd5c
HOzy-Joon <danielisdan@naver.com>
2023-02-17 02:48:11 +0900

    230216 covered

--- contents/ch_03/problems/practice/43.c
+++ contents/ch_03/problems/practice/43.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()  {
-    int i;
-    int c;
-    char *dest = malloc(sizeof(char) * 8);
-    char *dest2 = dest;
-    while ((c = getchar()) != '\n' && c != EOF){
-        printf("%d , %c\n", c, c);
-        *dest++ = c;
-    }
-    printf("\n\n");
-    for (i=0; i<8; i++){
-        printf("%d, %c\n", *dest2, *dest2);
-        dest2++;
-    }
+// int main()  {
+//     int i;
+//     int c;
+//     char *dest = malloc(sizeof(char) * 8);
+//     char *dest2 = dest;
+//     while ((c = getchar()) != '\n' && c != EOF){
+//         printf("%d , %c\n", c, c);
+//         *dest++ = c;
+//     }
+//     printf("\n\n");
+//     for (i=0; i<8; i++){
+//         printf("%d, %c\n", *dest2, *dest2);
+//         dest2++;
+//     }
+// }
+
+
+int main(){
+    int local;
+    printf("%p", &local);
 }
